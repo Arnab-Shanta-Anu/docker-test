92adf4eb9f901e421c6372dbcb4e4659e82bfaa5
Arnab-Shanta-Anu <arnabshantaanu@gmail.com>
2022-10-25 18:26:34 +0600

    added docker-compose

--- backend/app.js
+++ backend/app.js
@@ -9,5 +9,12 @@ app.get("/", (req, res) => {
     msg: "hello world",
   });
 });
+app.get("/docker", (req, res) => {
+  res.json({
+    msg: "hello from docker",
+  });
+});
 
-app.listen(4000);
+app.listen(4000, () => {
+  console.log("listeing on port 4000...");
+});
